d9db25f232dbd3e30c0a444667354a166e739c16
Manav Kashyap <manavkashyap6708@gmail.com>
2025-03-19 21:34:40 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
-# This script will force reinstall PyMuPDF
-# It ensures the package is installed properly
-pip install --no-cache-dir --force-reinstall PyMuPDF==1.24.0
+#!/bin/bash
+
+# Ensure pip is up-to-date
+pip install --upgrade pip  
+
+# Force reinstall Gunicorn
+pip install --no-cache-dir --force-reinstall gunicorn  
+
+# Force reinstall PyMuPDF
+pip install --no-cache-dir --force-reinstall PyMuPDF==1.24.0  
