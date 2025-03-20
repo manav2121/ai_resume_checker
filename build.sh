d5fe08b1423cfb6158ed14e539f793d2c7692123
Manav Kashyap <manavkashyap6708@gmail.com>
2025-03-20 12:22:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-# Ensure pip is up-to-date
-pip install --upgrade pip  
-
-# Install dependencies (force reinstall to avoid issues)
-pip install --no-cache-dir --force-reinstall flask gunicorn PyMuPDF
+pip install --upgrade pip
+pip install -r requirements.txt
+pip install PyMuPDF==1.24.0
